359468bfabda6da30892271bf6f2314a5ba20d05
Nikhil Gosavi <gosavinikhil2789@gmail.com>
2025-01-26 12:38:11 +0530

    Add files via upload

---
+++ Avg.js
@@ -0,0 +1,17 @@
+// let A , B;
+// A = 150 ,560, 800;
+// B = 140 , 698, 635;
+
+// let avg = (A+B)/3;
+
+// console.log(avg);
+
+let A , B;
+ A = 150 ,560, 800;
+ B = 140 , 698, 635;
+
+ let avg_A = (150+560+800)/3;
+ let avg_B = (140+698+635)/3;
+
+ console.log(avg_A);
+ console.log(avg_B);
